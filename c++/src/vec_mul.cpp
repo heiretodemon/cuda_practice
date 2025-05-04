d232ca3d44e69fb7b05f0c541d6bec5e78b2a07d
heiretodemon <171982919@qq.com>
2025-05-05 01:11:57 +0800

    sgemm_naive

---
+++ c++/src/vec_mul.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+#define OFFSET(row, col, ld) ((row) * (ld) + (col))
+
+void cpuSgemm(
+    float *a, float *b, float *c, const int M, const int N, const int K) {
+
+    for (int m = 0; m < M; m++) {
+        for (int n = 0; n < N; n++) {
+            float psum = 0.0;
+            for (int k = 0; k < K; k++) {
+                psum += a[OFFSET(m, k, K)] * b[OFFSET(k, n, N)];
+            }
+            c[OFFSET(m, n, N)] = psum;
+        }
+    }
+}
+
+int main(){
+    cpuSgemm;
+
+    return 0;
+}
